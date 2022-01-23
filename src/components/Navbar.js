796ccf8659188a502de9fb8581ea97c68119344e
AntoAlvarezG <antoalvarez7@gmail.com>
2022-01-23 20:13:15 -0300

    Fixed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar () {
     return (
         <nav>
-            <img className="nav-logo" src="../images/logo.png" alt="world icon" />
+            <img className="nav-logo" src="./images/logo.png" alt="world icon" />
             <h3>my travel journal.</h3>
         </nav>
     )
