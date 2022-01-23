3bb0688ba72cf1bb213198024ba2bc41b0f84757
AntoAlvarezG <antoalvarez7@gmail.com>
2022-01-23 20:09:13 -0300

    Fixed images src

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 export default function Card (props) {
     return (
         <div className="card-container">
-            <img className="card-img" src={`../images/${props.entry.img}`} alt="some place" />
+            <img className="card-img" src={`./images/${props.entry.img}`} alt="some place" />
             <div className="card-info">
                 <div className="location-info">
-                    <img className="location-icon" src="../images/location-icon.png" alt="icon" />
+                    <img className="location-icon" src="./images/location-icon.png" alt="icon" />
                     <span className="location">{props.entry.location}</span>
                     <a href={props.entry.maps} target="_blank" rel="noreferrer">
                         <span className="maps">View on Google Maps</span>
